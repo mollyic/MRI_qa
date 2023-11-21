880026cb84b56f2f841baf807af67775c93647e4
Molly Ireland <molly.ireland@florey.edu.au>
2023-11-21 15:34:31 +1100

    new config updates

--- environ/__init__.py
+++
@@ -1,9 +0,0 @@
-from environ.parser import parse_console, parse_usrenv
-from environ.updater import configupdate
-#from environ.updater import terminalOptions
-
-__all__= [
-    "parse_console", 
-    "configupdate", 
-    "parse_usrenv"
-]
--- review/__init__.py
+++
@@ -1,8 +0,0 @@
-from review.base import review_artifacts, review_subject, kill_process, collector
-
-__all__= [
-    "review_artifacts", 
-    "review_subject", 
-    "kill_process", 
-    "collector", 
-]
